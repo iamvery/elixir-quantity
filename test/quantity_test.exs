3c579d445c8acafece375c8c41a9503aa8b1fc57
Jay Hayes <ur@iamvery.com>
2015-07-20 16:13:53 -0500

    Remove implementation, tag tests with :skip
    * Run tests with:
      $ mix test --exclude skip

--- test/quantity_test.exs
+++ test/quantity_test.exs
@@ -1,10 +1,12 @@
 defmodule QuantityTest do
   use ExUnit.Case
 
+  @tag :skip
   test "equality" do
     assert Quantity.of(2, :inches) == Quantity.of(2, :inches)
   end
 
+  @tag :skip
   test "self conversion" do
     length = Quantity.of(2, :feet)
     assert Quantity.of(length, :feet) == length
@@ -13,29 +15,34 @@ defmodule QuantityTest do
     assert Quantity.of(length, :inches) == length
   end
 
+  @tag :skip
   test "convert feet to inches" do
     length = Quantity.of(3, :feet)
     length_in_inches = Quantity.of(length, :inches)
     assert length_in_inches == Quantity.of(36, :inches)
   end
 
+  @tag :skip
   test "convert inches to feet" do
     length = Quantity.of(12, :inches)
     length_in_feet = Quantity.of(length, :feet)
     assert length_in_feet == Quantity.of(1, :feet)
   end
 
+  @tag :skip
   test "adding quantities of like units" do
     length = Quantity.of(2, :feet)
     assert Quantity.add(length, length) == Quantity.of(4, :feet)
   end
 
+  @tag :skip
   test "adding quantities of differing units" do
     first = Quantity.of(2, :feet)
     second = Quantity.of(12, :inches)
     assert Quantity.add(first, second) == Quantity.of(3, :feet)
   end
 
+  @tag :skip
   test "string conversion" do
     length = Quantity.of(3, :feet)
     assert "#{length}" == "3 feet"
@@ -44,6 +51,7 @@ defmodule QuantityTest do
     assert "#{length}" == "12 inches"
   end
 
+  @tag :skip
   test "inflection" do
     length = Quantity.of(1, :feet)
     assert "#{length}" == "1 foot"
