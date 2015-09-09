ee31df3d869b67d4b94469a7628e7909d1e1a2b3
Jay Hayes <ur@iamvery.com>
2015-09-09 17:39:48 -0500

    Restructure some little things

--- lib/quantity.ex
+++ lib/quantity.ex
@@ -2,14 +2,10 @@ defmodule Quantity do
   defstruct magnitude: 0, unit: :unknown
 
   def of(%{unit: unit} = quantity, unit), do: quantity
-  def of(%{magnitude: magnitude, unit: current_unit}, new_unit) do
-    new_magnitude = convert(magnitude, current_unit, new_unit)
-    of(new_magnitude, new_unit)
+  def of(%{magnitude: value, unit: current_unit}, new_unit) do
+    convert(value, current_unit, new_unit) |> of(new_unit)
   end
-
-  def of(magnitude, unit) do
-    %Quantity{magnitude: magnitude, unit: unit}
-  end
+  def of(value, unit), do: %Quantity{magnitude: value, unit: unit}
 
   def convert(value, :feet, :inches), do: value * 12 # inches per foot
   def convert(value, :inches, :feet), do: value / 12 # inches per foot
@@ -17,10 +13,7 @@ defmodule Quantity do
   def add(%{unit: unit} = first, %{unit: unit} = second) do
     of(first.magnitude + second.magnitude, unit)
   end
-
-  def add(%{unit: first_unit} = first, second) do
-    add(first, of(second, first_unit))
-  end
+  def add(first, second), do: of(second, first.unit) |> add(first)
 
   def inflect(%{magnitude: magnitude, unit: unit}) when magnitude == 1 do
     Inflection.singularize(unit)
